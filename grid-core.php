bb830e5eaab93564af8b4f85ebf9b67fc7712208
Philipp Zedler <philipp@neue-musik.com>
2017-05-24 12:13:59 +0200

    Allow this to be a Wordpress plugin

---
+++ grid-core.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Grid Core
+* Plugin URI: https://github.com/philippze/grid
+* Description: The code required for the Wordpress Grid plugin to work
+* Version: 1.0
+* Author: Palasthotel <rezeption@palasthotel.de>
+* Author URI: https://palasthotel.de/
+* License: GPLv3
+*/
